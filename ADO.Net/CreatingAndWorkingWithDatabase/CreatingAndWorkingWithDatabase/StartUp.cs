94e0ed96b20e8423f7bcbbd983f8062ceb3f980d
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-02-18 18:11:54 +0200

    creating first task

---
+++ ADO.Net/CreatingAndWorkingWithDatabase/CreatingAndWorkingWithDatabase/StartUp.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace CreatingAndWorkingWithDatabase
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string sqlConncetionString1 = @"Data Source=DESKTOP-8F63LT6\TEW_SQLEXPRESS;Database=master;Integrated Security=true";
+            string baseName = "MinionsDB";
+            string dataBaseName = $"CREATE DATABASE {baseName}";
+            using (var conncetion = new SqlConnection(sqlConncetionString1))
+            {
+                conncetion.Open();
+
+                ExecudeQuery(dataBaseName, conncetion); // part 1
+
+                conncetion.Close();
+            }
+
+            string sqlConncetionString2 = @"Data Source=DESKTOP-8F63LT6\TEW_SQLEXPRESS;Database=" + baseName + ";Integrated Security=true";
+            using (var conncetion = new SqlConnection(sqlConncetionString2))
+            {
+                string[] tables = Tables(); // part 2
+                foreach (string table in tables)
+                {
+                    ExecudeQuery(table, conncetion);
+                }
+
+                string[] tableInformation = TableInformation(); // part 3
+                foreach (string tableInfo in tableInformation)
+                {
+                    ExecudeQuery(tableInfo, conncetion);
+                }
+            }
+        }
+
+        private static void ExecudeQuery(string query, SqlConnection connection)
+        {
+            using (var comand = new SqlCommand(query, connection))
+            {
+                comand.ExecuteNonQuery();
+            }
+        }
+
+        private static string[] TableInformation()
+        {
+            string[] result = new string[]
+            {
+                "INSERT INTO Countries ([Name]) VALUES ('Bulgaria'),('England'),('Cyprus'),('Germany'),('Norway')",
+                "INSERT INTO Towns ([Name], CountryCode) VALUES ('Plovdiv', 1),('Varna', 1),('Burgas', 1),('Sofia', 1),('London', 2),('Southampton', 2),('Bath', 2),('Liverpool', 2),('Berlin', 3),('Frankfurt', 3),('Oslo', 4)",
+                "INSERT INTO Minions (Name,Age, TownId) VALUES('Bob', 42, 3),('Kevin', 1, 1),('Bob ', 32, 6),('Simon', 45, 3),('Cathleen', 11, 2),('Carry ', 50, 10),('Becky', 125, 5),('Mars', 21, 1),('Misho', 5, 10),('Zoe', 125, 5),('Json', 21, 1)",
+                "INSERT INTO EvilnessFactors (Name) VALUES ('Super good'),('Good'),('Bad'), ('Evil'),('Super evil')",
+                "INSERT INTO Villains (Name, EvilnessFactorId) VALUES ('Gru',2),('Victor',1),('Jilly',3),('Miro',4),('Rosen',5),('Dimityr',1),('Dobromir',2)",
+                "INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (4,2),(1,1),(5,7),(3,5),(2,6),(11,5),(8,4),(9,7),(7,1),(1,3),(7,3),(5,3),(4,3),(1,2),(2,1),(2,7)"
+            };
+
+            return result;
+        }
+
+        private static string[] Tables()
+        {
+            string[] result = new string[]
+            {
+                "CREATE TABLE Countries(Id INT PRIMARY KEY IDENTITY, Name NVARCHAR(250))",
+                "CREATE TABLE Towns(	Id INT PRIMARY KEY IDENTITY, Name NVARCHAR(250), CountryCode INT FOREIGN KEY REFERENCES Countries(Id))",
+                "CREATE TABLE Minions(Id INT PRIMARY KEY IDENTITY, Name NVARCHAR(250),	Age INT,	TownId INT FOREIGN KEY REFERENCES Towns(Id))",
+                "CREATE TABLE EvilnessFactors(Id INT PRIMARY KEY IDENTITY,	Name NVARCHAR(250))",
+                "CREATE TABLE Villains(Id INT PRIMARY KEY IDENTITY,	Name NVARCHAR(250),	EvilnessFactorId INT FOREIGN KEY REFERENCES EvilnessFactors(Id))",
+                "CREATE TABLE MinionsVillains(MinionId INT FOREIGN KEY REFERENCES Minions(Id),	VillainId INT FOREIGN KEY REFERENCES Villains(Id), PRIMARY KEY (MinionId,VillainId))"
+            };
+
+            return result;
+        }
+    }
+}
