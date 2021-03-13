64fdbe165ce260f8ab7b2daf7536ce64c5a5abe3
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-03-13 10:40:21 +0200

    Till task 3

---
+++ JSON/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/Links/JSONLinks.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductShop.Links
+{
+    public static class JSONLinks
+    {
+        public const string users = "../../../Datasets/users.json";
+        public const string products = "../../../Datasets/products.json";
+        public const string categoriesToProducts = "../../../Datasets/categories-products.json";
+        public const string categories = "../../../Datasets/categories.json";
+    }
+}
--- JSON/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
+++ JSON/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 using ProductShop.Data;
+using ProductShop.Links;
 using ProductShop.Models;
 
 namespace ProductShop
@@ -13,14 +14,18 @@ namespace ProductShop
         public static void Main(string[] args)
         {
             ProductShopContext db = new ProductShopContext();
+            string result = string.Empty;
             //ResetDatabase(db);
-            string json = File.ReadAllText("../../../Datasets/users.json"); // Task 1
-            string result = ImportUsers(db, json); // Task 1
-
+            //
+            //string jsonUsers = File.ReadAllText(JSONLinks.users); // Task 1
+            //result = ImportUsers(db, jsonUsers); // Task 1
+            //string jsonProducts = File.ReadAllText(JSONLinks.products); // Task 2
+            //result = ImportProducts(db, jsonProducts); // Task 2
+            string jsonCategories = File.ReadAllText(JSONLinks.categories); // Task 3
+            result = ImportCategories(db, jsonCategories); // Task 3
 
             Console.WriteLine(result);
         }
-
         public static void ResetDatabase(ProductShopContext db)
         {
             db.Database.EnsureDeleted();
@@ -37,6 +42,25 @@ namespace ProductShop
             context.SaveChanges();
 
             return $"Successfully imported {objUser.Length}";
+        }
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var objProducts = JsonConvert.DeserializeObject<Product[]>(inputJson);
+            context.AddRange(objProducts);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {objProducts.Length}";
+        }
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+
+            var objcategories = JsonConvert.DeserializeObject<Category[]>(inputJson).Where(c=> c.Name != null).ToArray();
+            context.AddRange(objcategories);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {objcategories.Length}";
         }
     }
 }
