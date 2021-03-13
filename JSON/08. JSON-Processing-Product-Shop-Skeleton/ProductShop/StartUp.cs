0765fb4ea0ce712e8c5e665f7bd4789753fbc857
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-03-13 09:39:06 +0200

    JSON Practiice

---
+++ JSON/08. JSON-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            ProductShopContext db = new ProductShopContext();
+            //ResetDatabase(db);
+            string json = File.ReadAllText("../../../Datasets/users.json"); // Task 1
+            string result = ImportUsers(db, json); // Task 1
+
+
+            Console.WriteLine(result);
+        }
+
+        public static void ResetDatabase(ProductShopContext db)
+        {
+            db.Database.EnsureDeleted();
+            Console.WriteLine("Deleted!!!");
+            db.Database.EnsureCreated();
+            Console.WriteLine("Created!!!");
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var objUser = JsonConvert.DeserializeObject<User[]>(inputJson);
+            context.AddRange(objUser);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {objUser.Length}";
+        }
+    }
+}
