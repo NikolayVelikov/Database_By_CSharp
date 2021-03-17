2ae4d28389a2d48ca2e28f769a2fdcb4f822ea4f
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-03-17 23:42:59 +0200

    Solutions for task 14 and 15

--- XML/09. XML-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
+++ XML/09. XML-Processing-Car-Dealer-Skeleton/CarDealer/StartUp.cs
@@ -7,6 +7,7 @@ using System.Xml.Serialization;
 using CarDealer.Concerter;
 using CarDealer.Data;
 using CarDealer.DataTransferObjects.Input;
+using CarDealer.DataTransferObjects.Output;
 using CarDealer.Models;
 
 namespace CarDealer
@@ -18,17 +19,62 @@ namespace CarDealer
             CarDealerContext db = new CarDealerContext();
             string result = string.Empty;
             //ResetDatabase(db);
-
+            //
             //var supplierXML = File.ReadAllText("./Datasets/suppliers.xml");
             //result = ImportSuppliers(db, supplierXML);
             //var partsXML = File.ReadAllText("./Datasets/parts.xml");
             //result = ImportParts(db, partsXML);
-            var carsXML = File.ReadAllText("./Datasets/cars.xml");
-            result = ImportCars(db, carsXML);
+            //var carsXML = File.ReadAllText("./Datasets/cars.xml");
+            //result = ImportCars(db, carsXML);
+            //var customersXml = File.ReadAllText("./Datasets/customers.xml");
+            //result = ImportCustomers(db, customersXml);
+            //var salesXml = File.ReadAllText("./Datasets/sales.xml");
+            //result = ImportSales(db, salesXml);
+
+            //result = GetCarsWithDistance(db);
+            result = GetCarsFromMakeBmw(db);
+
 
             Console.WriteLine(result);
-
         }
+        public static string GetCarsWithDistance(CarDealerContext context)
+        {
+            int distace = 2000000;
+            var cars = context.Cars
+                .Where(d => d.TravelledDistance > distace)
+                .Select(x => new CarOutputModel
+                {
+                    Make = x.Make,
+                    Model = x.Model,
+                    TravelledDistance = x.TravelledDistance
+                })
+                .OrderBy(x => x.Make).ThenBy(x => x.Model).Take(10).ToArray();
+
+            string root = "cars";
+            var carsXml = XmlConverter.Serialize<CarOutputModel>(cars, root);
+
+            return carsXml;
+        }
+        public static string GetCarsFromMakeBmw(CarDealerContext context)
+        {
+            string make = "BMW";
+
+            var bmwCars = context.Cars
+                .Where(x => x.Make.ToUpper() == make)
+                .Select(x => new BMWOutputModel
+                {
+                    Id = x.Id,
+                    Model = x.Model,
+                    TravelledDistance = x.TravelledDistance
+                })
+                .OrderBy(x => x.Model).ThenByDescending(x => x.TravelledDistance).ToArray();
+
+            string root = "cars";
+            var bmwCarsXml = XmlConverter.Serialize<BMWOutputModel>(bmwCars, root);
+
+            return bmwCarsXml;
+        }
+
         private static void ResetDatabase(CarDealerContext context)
         {
             context.Database.EnsureDeleted();
@@ -36,6 +82,7 @@ namespace CarDealer
             context.Database.EnsureCreated();
             Console.WriteLine("Created!!!");
         }
+
         public static string ImportSuppliers(CarDealerContext context, string inputXml)
         {
             string root = "Suppliers";
@@ -108,8 +155,44 @@ namespace CarDealer
 
             context.AddRange(cars);
             context.SaveChanges();
-            
+
             return $"Successfully imported {cars.Count}";
+        }
+        public static string ImportCustomers(CarDealerContext context, string inputXml)
+        {
+            string root = "Customers";
+            var xmlObjects = XmlConverter.Deserializer<CustomersInputModel>(inputXml, root);
+
+            var customers = xmlObjects.Select(x => new Customer
+            {
+                Name = x.Name,
+                BirthDate = x.BirthDate,
+                IsYoungDriver = x.IsYoungDriver
+            }).ToArray();
+
+            context.AddRange(customers);
+            context.SaveChanges();
+
+
+            return $"Successfully imported {customers.Length}";
+        }
+        public static string ImportSales(CarDealerContext context, string inputXml)
+        {
+            var root = "Sales";
+            var existedCars = context.Cars.Select(x => x.Id).ToArray();
+            var customersObjects = XmlConverter.Deserializer<SalesInputModel>(inputXml, root);
+
+            var sales = customersObjects.Where(x => existedCars.Contains(x.CarId)).Select(x => new Sale
+            {
+                Discount = x.Discount,
+                CarId = x.CarId,
+                CustomerId = x.CustomerId
+            }).ToArray();
+
+            context.AddRange(sales);
+            context.SaveChanges();
+
+            return $"Successfully imported {sales.Length}";
         }
     }
 }
