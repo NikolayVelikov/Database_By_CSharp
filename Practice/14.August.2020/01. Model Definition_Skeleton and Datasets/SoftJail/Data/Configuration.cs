feadf6321e5705ddfa52982bdb6ec895e863e38f
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-03-27 09:44:21 +0200

    Drop down
    Deleting the project

--- Practice/14.August.2020/01. Model Definition_Skeleton and Datasets/SoftJail/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SoftJail.Data
-{
-   public static class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-8F63LT6\TEW_SQLEXPRESS;Database=SoftJail;Trusted_Connection=True";
-    }
-}
