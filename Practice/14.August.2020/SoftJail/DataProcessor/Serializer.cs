c3a66fe171858e3ab7dfcd35c8a795646c7fb082
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-03-27 18:15:55 +0200

    Export JSON

--- Practice/14.August.2020/SoftJail/DataProcessor/Serializer.cs
+++ Practice/14.August.2020/SoftJail/DataProcessor/Serializer.cs
@@ -2,13 +2,33 @@
 {
 
     using Data;
+    using Newtonsoft.Json;
     using System;
+    using System.Linq;
 
     public class Serializer
     {
         public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
         {
-            throw new NotImplementedException();
+            var prisoners = context.Prisoners
+                        .Where(p => ids.Contains(p.Id))
+                        .Select(p => new
+                        {
+                            Id = p.Id,
+                            Name = p.FullName,
+                            CellNumber = p.Cell.CellNumber,
+                            Officers = p.PrisonerOfficers.Select(of => new
+                            {
+                                OfficerName = of.Officer.FullName,
+                                Department = of.Officer.Department.Name
+                            }).OrderBy(of => of.OfficerName).ToList(),
+
+                            TotalOfficerSalary = decimal.Parse(p.PrisonerOfficers.Sum(of => of.Officer.Salary).ToString("F2"))
+                        }).OrderBy(p => p.Name).ThenBy(p => p.Id).ToList();
+
+            var prisonerJson = JsonConvert.SerializeObject(prisoners, Formatting.Indented);
+
+            return prisonerJson;
         }
 
         public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
