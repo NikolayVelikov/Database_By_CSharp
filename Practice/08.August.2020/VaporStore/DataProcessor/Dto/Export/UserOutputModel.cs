27011ea1cb3e26896add3157138f535631fdef71
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-03-29 23:05:39 +0300

    Creating the queries

---
+++ Practice/08.August.2020/VaporStore/DataProcessor/Dto/Export/UserOutputModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace VaporStore.DataProcessor.Dto.Export
+{
+    [XmlType("User")]
+    public class UserOutputModel
+    {
+        
+        [XmlAttribute("username")]
+        public string UserName { get; set; }
+
+        [XmlArray("Purchases")]
+        public PurchaseOutpuModel[] Purchases { get; set; }
+
+        [XmlElement("TotalSpent")]
+        public decimal TotalSpent { get; set; }
+    }
+}
--- Practice/08.August.2020/VaporStore/DataProcessor/Serializer.cs
+++ Practice/08.August.2020/VaporStore/DataProcessor/Serializer.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Linq;
     using Data;
     using Newtonsoft.Json;
@@ -11,32 +12,61 @@
     {
         public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
         {
-            var game = context.Genres
-                    .Where(g => genreNames.Contains(g.Name) && g.Games.Select(g => g.Purchases.Any()).Count() != 0)
-                    .Select(g => new
+            var games = context.Genres.ToList()
+                .Where(x => genreNames.Contains(x.Name))
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Genre = x.Name,
+                    Games = x.Games.Select(g => new
                     {
                         Id = g.Id,
-                        Genre = g.Name,
-                        Games = g.Games
-                        .Select(g => new
-                        {
-                            Id = g.Id,
-                            Title = g.Name,
-                            Developer = g.Developer.Name,
-                            Tags = g.GameTags.Select(t => t.Tag.Name).ToList(),
-                            Players = g.Purchases.Count
-                        }).OrderByDescending(x => x.Players).ThenBy(x => x.Id).ToList(),
-                        TotalPlayers = g.Games.Select(x => new { number = x.Purchases.Count() }).Count()
-                    }).OrderByDescending(x => x.TotalPlayers).ThenBy(x => x.Id).ToList();
+                        Title = g.Name,
+                        Developer = g.Developer.Name,
+                        Tags = string.Join(", ", g.GameTags.Select(t => t.Tag.Name)),
+                        Players = g.Purchases.Count()
+                    })
+                    .Where(g => g.Players > 0)
+                    .OrderByDescending(p => p.Players)
+                    .ThenBy(g => g.Id),
+                    TotalPlayers = x.Games.Sum(g => g.Purchases.Count())
+                }).OrderByDescending(x => x.TotalPlayers).ThenBy(x => x.Id);
 
-            var gamesJson = JsonConvert.SerializeObject(game, Formatting.Indented);
+
+
+            var gamesJson = JsonConvert.SerializeObject(games, Formatting.Indented);
 
             return gamesJson;
         }
 
         public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
         {
-            throw new NotImplementedException();
+            var data = context.Users.ToList()
+                .Where(x => x.Cards.Any(c => c.Purchases.Any(p=> p.Type.ToString() == storeType)))
+                .Select(x => new UserOutputModel()
+                {
+                    UserName = x.Username,
+                    TotalSpent = x.Cards.Sum(c => c.Purchases.Where(p => p.Type.ToString() == storeType).Sum(p => p.Game.Price)),
+                    Purchases = x.Cards.SelectMany(c => c.Purchases)
+                    .Where(p=> p.Type.ToString() == storeType)
+                    .Select(p => new PurchaseOutpuModel()
+                    {
+                        Cvc = p.Card.Cvc,
+                        Card = p.Card.Number,
+                        Date = p.Date.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                        Game = new GameOutputModel()
+                        {
+                            Genre = p.Game.Genre.Name,
+                            Price = p.Game.Price,
+                            Title = p.Game.Name
+                        }
+                    })
+                    .OrderByDescending(x=> x.Date).ToArray()
+                }).OrderByDescending(x => x.TotalSpent).ThenBy(x => x.UserName).ToArray();
+
+            var dataXml = XmlConverter.Serialize<UserOutputModel>(data, "Users");
+
+            return dataXml;
         }
     }
 }
