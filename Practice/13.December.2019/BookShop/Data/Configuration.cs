7b0fa43c667d43e4bebc3e9778ece0071b0c8356
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-03-30 19:47:01 +0300

    Creating DB

---
+++ Practice/13.December.2019/BookShop/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BookShop.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-8F63LT6\TEW_SQLEXPRESS;Database=BookShop;Trusted_Connection=True";
+    }
+}
---
+++ Practice/13.December.2019/BookShop/Data/Models/AuthorBook.cs
@@ -0,0 +1,20 @@
+﻿namespace BookShop.Data.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class AuthorBook
+    {
+        [ForeignKey(nameof(Author))]
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+
+        [ForeignKey(nameof(Book))]
+        public int BookId { get; set; }
+        public virtual Book Book { get; set; }
+    }
+}
+
+//•	AuthorId - integer, Primary Key, Foreign key (required)
+//•	Author - Author
+//•	BookId - integer, Primary Key, Foreign key (required)
+//•	Book - Book
