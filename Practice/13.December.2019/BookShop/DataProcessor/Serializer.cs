d7d5b2dbc282e0e5d9878be98c59c9d83ca64df8
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-03-30 23:37:53 +0300

    Creating exports

--- Practice/13.December.2019/BookShop/DataProcessor/Serializer.cs
+++ Practice/13.December.2019/BookShop/DataProcessor/Serializer.cs
@@ -7,6 +7,8 @@
     using System.Text;
     using System.Xml;
     using System.Xml.Serialization;
+    using BookShop.Data.Models.Enums;
+    using BookShop.DataProcessor.ExportDto;
     using Data;
     using Newtonsoft.Json;
     using Formatting = Newtonsoft.Json.Formatting;
@@ -15,26 +17,46 @@
     {
         public static string ExportMostCraziestAuthors(BookShopContext context)
         {
-            var authors = context.Authors
+            var authors = context.Authors.ToList()
                 .Select(a => new
                 {
                     AuthorName = a.FirstName + ' ' + a.LastName,
                     Books = a.AuthorsBooks
+                    .OrderByDescending(b=> b.Book.Price)
                     .Select(b => new
                     {
                         BookName = b.Book.Name,
                         BookPrice = b.Book.Price.ToString("f2")
-                    }).OrderByDescending(b => b.BookPrice).ToArray()
-                }).OrderByDescending(a => a.Books.Count()).ThenBy(a => a.AuthorName).ToArray();
+                    }).ToArray()
+                })
+                .ToArray()
+                .OrderByDescending(a => a.Books.Length)
+                .ThenBy(a => a.AuthorName);
 
-            var authorsJson = JsonConvert.SerializeObject(authors,Formatting.Indented);
+            var authorsJson = JsonConvert.SerializeObject(authors, Formatting.Indented);
 
             return authorsJson;
         }
 
         public static string ExportOldestBooks(BookShopContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            Genre genre = Enum.Parse<Genre>("science",true);
+            var books = context.Books
+                .Where(x => x.PublishedOn <= date && x.Genre == genre)                
+                .Select(x => new BookOutputModel()
+                {
+                    Pages = x.Pages.ToString(),
+                    Name = x.Name,
+                    Date = x.PublishedOn.ToString("MM/dd/yyyy")
+                })
+                .OrderByDescending(x => int.Parse(x.Pages))
+                .ThenByDescending(x => x.Date).Take(10).ToArray();
+
+            var a = books.FirstOrDefault(x => x.Name == "Palo Blanco");
+            string root = "Books";
+            var booksXml = XmlConverter.Serialize<BookOutputModel>(books, root);
+
+            return booksXml;
         }
     }
 }
